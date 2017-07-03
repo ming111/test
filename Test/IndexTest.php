7206453c0f426cc9d3066ec67ef34dea98dd4d91
ming111 <xingming.wang@guanaitong.com>
2017-07-03 20:59:46 +0800

    phpunit

--- Test/IndexTest.php
+++ Test/IndexTest.php
@@ -8,10 +8,13 @@ class IndexTest extends PHPUnit_Framework_TestCase
     public function setUp()
     {}
 
-    public function testIndex()
+    /**
+     * @dataProvider additionProviderIndex
+     */
+    public function testIndex($a,$b)
     {
         $index = new Index();
-        $result = $index->index();
+        $result = $index->index($a,$b);
         $this->assertSame(true,$result);
     }
 
@@ -20,5 +23,16 @@ class IndexTest extends PHPUnit_Framework_TestCase
         $index = new Index();
         $result = $index->test();
         $this->assertSame(true,$result);
+    }
+
+    //数据供给器
+    public function additionProviderIndex()
+    {
+        return [
+            [1,1],
+            [2,8],
+            [3,5],
+            [8,8]
+        ];
     }
 }
